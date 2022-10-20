7043d99a356720edc503c1044e311ec8a8ffe551
Tahir Fayyaz <tahir.fayyaz@databricks.com>
2022-10-20 13:44:46 +0000

    updated to use ga campaign_events only

--- dbt/models/marts/product/agg_festival_country_metrics.sql
+++ dbt/models/marts/product/agg_festival_country_metrics.sql
@@ -1,8 +1,8 @@
 {{ config(materialized='table') }}
 
-with events as (
+with campaign_events as (
 
-    select * from {{ ref('fct_events') }}
+    select * from {{ ref('fct_campaign_events') }}
 
 ),
 festivals as (
@@ -15,10 +15,10 @@ select
     date, 
     f.festival_country as festival_country,
     f.festival_city as festival_city,
-    sum(if(event_name='festival_view', event_count, null)) as festival_views,
-    sum(if(event_name='video_play', event_count, null)) as video_plays,
-    sum(if(event_name='tickets_link', event_count, null)) as tickets_link_clicked
-from events as e
+    sum(festival_views) as festival_views,
+    sum(video_plays) as video_plays,
+    sum(tickets_link_clicked) as tickets_link_clicked
+from campaign_events as e
 inner join festivals as f
 on e.festival_id = f.festival_id
 group by date, festival_country, festival_city
